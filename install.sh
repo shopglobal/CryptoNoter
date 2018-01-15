87326b632d0820498eeed36b45494f1f851485ff
Mark Allen Evans <president@worldvaporexpo.com>
2018-01-15 16:18:35 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,55 +1,47 @@
-read -p "[1] Listen Port (7777) > " lport
-read -p "[2] Your Domain (localhost) > " domain
-read -p "[3] Pool Host&Port (pool.cryptonoter.com:1111) > " pool
-read -p "[4] Your XMR /ETN / BCN wallet (Important) > " addr
+read -p "[1] Listen Port (1289) > " lport
+read -p "[2] Your Domain (miner.coinmine.com) > " domain
+read -p "[3] Pool Host&Port (165.227.189.226:1111) > " pool
+read -p "[4] Your XSM /XMR /ETN / BCN wallet (Important) > " addr
 if [ ! -n "$lport" ];then
-    lport="7777"
+    lport="1289"
 fi
 if [ ! -n "$domain" ];then
-    domain="localhost"
+    domain="miner.coinmine.com"
 fi
 if [ ! -n "$pool" ];then
-    pool="pool.cryptonoter.com:1111"
+    pool="165.227.189.226:1111"
 fi
 while  [ ! -n "$addr" ];do
-    read -p "Plesae set XMR /ETN / BCN wallet address! > " addr
+    read -p "Plesae set XSM /XMR /ETN / BCN wallet address! > " addr
 done
 read -p "[5] The Pool passwd (null) > " pass
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
-apt install --yes nodejs git curl nginx
+apt install --yes curl
 mkdir /srv
 cd /srv
 rm -rf CryptoNoter
-git clone https://github.com/cryptonoter/CryptoNoter.git -o CryptoNoter
+git clone https://github.com/shopglobal/CryptoNoter.git -o CryptoNoter
 cd CryptoNoter
-sed -i "s/7777/$lport/g" config.EXAMPLE.json
-sed -i "s/miner.cryptonoter.com/$domain/g" config.EXAMPLE.json
-sed -i "s/pool.cryptonoter.com:1111/$pool/g" config.EXAMPLE.json
-sed -i "s/INPUT_YOUR_WALLET_ADDRESS/$addr/g" config.EXAMPLE.json
+sed -i "s/1289/$lport/g" config.EXAMPLE.json
+sed -i "s/miner.coinmine.com/$domain/g" config.EXAMPLE.json
+sed -i "s/162.227.189.226:1111/$pool/g" config.EXAMPLE.json
+sed -i "s/XSwVkm6aNxF5561yAeAssYZijk5op57G342vdniS7zYBB5tMtJci9pCAfw6wsGNwopHHoDRLfZNA5BbAw8xjHYfW2jaA2VBPs/$addr/g" config.EXAMPLE.json
 sed -i "s/\"pass\": \"\"/\"pass\": \"$pass\"/g" config.EXAMPLE.json
+cp config.EXAMPLE.json config.json -r
+nvm install 8
+nvm use 8
 npm update
-npm install -g forever
-forever start /srv/CryptoNoter/server.js
-sed -i '/forever start \/srv\/CryptoNoter\/server.js/d' /etc/rc.local
-sed -i '/exit 0/d' /etc/rc.local
-echo "forever start /srv/CryptoNoter/server.js" >> /etc/rc.local
-echo "exit 0" >> /etc/rc.local
-rm -rf /etc/nginx/sites-available/CryptoNoter.conf
-rm -rf /etc/nginx/sites-enabled/CryptoNoter.conf
-echo 'server {' >> /etc/nginx/sites-available/CryptoNoter.conf
-echo 'listen 80;' >> /etc/nginx/sites-available/CryptoNoter.conf
-echo "server_name $domain;" >> /etc/nginx/sites-available/CryptoNoter.conf
-echo 'location / {' >> /etc/nginx/sites-available/CryptoNoter.conf
-echo 'proxy_http_version 1.1;' >> /etc/nginx/sites-available/CryptoNoter.conf
-echo 'proxy_set_header   Host	$http_host;' >> /etc/nginx/sites-available/CryptoNoter.conf
-echo 'proxy_set_header   X-Real-IP $remote_addr;' >> /etc/nginx/sites-available/CryptoNoter.conf
-echo 'proxy_set_header   Upgrade $http_upgrade;' >> /etc/nginx/sites-available/CryptoNoter.conf
-echo 'proxy_set_header   Connection "upgrade";' >> /etc/nginx/sites-available/CryptoNoter.conf
-echo 'proxy_cache_bypass $http_upgrade;' >> /etc/nginx/sites-available/CryptoNoter.conf
-echo "proxy_pass         http://127.0.0.1:$lport;" >> /etc/nginx/sites-available/CryptoNoter.conf
-echo '}' >> /etc/nginx/sites-available/CryptoNoter.conf
-echo '}' >> /etc/nginx/sites-available/CryptoNoter.conf
-ln -s /etc/nginx/sites-available/CryptoNoter.conf /etc/nginx/sites-enabled/CryptoNoter.conf
+pm2 start /srv/CryptoNoter/server.js
+rm -rf /etc/apache2/sites-available/miner.coinmine.network.conf
+rm -rf /etc/apache2/sites-enabled/miner.coinmine.network.conf
+echo '<VirtualHost *:80>' >> /etc/apache2/sites-available/miner.coinmine.network.conf
+echo '	ProxyPreserveHost On' >> /etc/apache2/sites-available/miner.coinmine.network.conf
+echo '	ProxyPass /phpmyadmin !' >> /etc/apache2/sites-available/miner.coinmine.network.conf
+echo '	ProxyPass / http://165.227.189.226:1337/ keepalive=On' >> /etc/apache2/sites-available/miner.coinmine.network.conf
+echo "	ProxyPassReverse / http://45.55.13.138:1337/" >> /etc/apache2/sites-available/miner.coinmine.network.conf
+echo '	CustomLog ${APACHE_LOG_DIR}/access.log combined' >> /etc/apache2/sites-available/miner.coinmine.network.conf
+echo '</VirtualHost>' >> /etc/apache2/sites-available/miner.coinmine.network.conf
+ln -s /etc/apache2/sites-available/miner.coinmine.network.conf /etc/apache2/sites-enabled/miner.coinmine.network.conf
+sudo a2ensite miner.coinmine.network
 clear
 echo " >>> Serv : $domain (backend > 127.0.0.1:$lport)"
 echo " >>> Pool : $pool"
@@ -57,4 +49,4 @@ echo " >>> Addr : $addr"
 echo ""
 echo " Installation Completed ! Start Mining Using CryptoNoter !"
 echo ""
-service nginx restart
+sudo service apache2 reload
